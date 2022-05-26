b874127430bcdd79e7f295dcd9761c907f1ea868
Empa <emanuel@empa.xyz>
2022-05-26 15:05:59 +0200

    Add some queries

---
+++ queries/sy/highlights.scm
@@ -0,0 +1,96 @@
+[
+  "and"
+  "or"
+  "not"
+] @keyword.operator
+
+[
+  "+"
+  "-"
+  "*"
+  "/"
+  "<="
+  "<"
+  ">="
+  ">"
+  "=="
+  "!="
+] @operator
+
+[
+  ":"
+  ","
+  "."
+] @punctuation.delimiter
+
+[
+  "("
+  ")"
+  "["
+  "]"
+  "{"
+  "}"
+ ] @punctation.bracket
+
+(comment) @comment
+
+(identifier) @variable
+
+(type) @type
+
+[
+  (int)
+  (float)
+] @number
+
+(nil) @constant.builtin
+
+(bool) @boolean
+
+(string) @string
+
+"ret" @keyword.return
+
+(function
+[
+  "fn"
+  "pu"
+  "do"
+  "end"
+] @keyword.function)
+
+(call name: (identifier) @function)
+(prim_call
+[
+  "'"
+] @operator)
+(arrow_call
+[
+  "->" 
+] @operator)
+
+(if
+[
+  "if"
+  "do"
+  "else"
+  "end"
+] @conditional)
+
+(case
+[
+  "case"
+  "->"
+  "do"
+  "else"
+  "end"
+] @conditional)
+
+(loop
+[
+  "loop"
+  "do"
+  "end"
+] @repeat)
+
+(this) @variable.builtin
